86dbd565b96e97ca799504c851b7835b64a11c82
Jonah Dlin <jonahdlin@MacBookAir5.local>
2017-09-29 16:09:36 -0400

    Added move event listener on nodes. Currently just logs something.

--- src/client/features/view/cy/events/index.js
+++ src/client/features/view/cy/events/index.js
@@ -1,9 +1,11 @@
 import bindHover from './hover';
 import bindExpandCollapse from './expandCollapse';
+import bindMove from './move';
 
 const bindEvents = (cy) => {
   bindHover(cy);
   bindExpandCollapse(cy);
+  bindMove(cy);
 };
 
 export default bindEvents;
