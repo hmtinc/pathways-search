3d28a573f163205e6b24ddda83ae77e73f5dee26
Geoffrey Elder <geoffreyelder@MacPro9.local>
2017-10-03 13:00:24 -0400

    Corrected error handling and formattting in updateVersion.js

--- src/server/updateVersion.js
+++ src/server/updateVersion.js
@@ -3,14 +3,14 @@
 // Link this to populateDB.js file xml file reading 
 
 
-process.on('unhandledRejection', up => {throw up}); // fail loudly. Enforces good promise handling
+process.on('unhandledRejection', up => { throw up; }); // fail loudly. Enforces good promise handling
 
 const r = require('rethinkdb');
 
 const dbName = 'layouts'; // easily allow testing on a fake database once the system is in use
 
-function insert(table, data, connection, callback){
-  return r.db(dbName).table(table).insert(data).run(connection,callback);
+function insert(table, data, connection, callback) {
+  return r.db(dbName).table(table).insert(data).run(connection, callback);
 }
 
 /* updateVersion(version,uris, data,connection,callback) creates the framework
@@ -18,21 +18,21 @@ function insert(table, data, connection, callback){
   It expects data to be a list of cytoscape objects, uris to be a list of 
   uris and version to be a simple version number.
  */
-function updateVersion(version, uris, data, connection,callback){
+function updateVersion(version, uris, data, connection, callback) {
   //Create a new version entry
-  var update = insert('version',{version:version},connection)
-  .then((result)=>{
-    //then iterate through each new data set and create the necessary tables.
-    var versionId = result.generated_keys[0];
-    for (var i = 0; i < uris.length; i++){
-      updateVersionEntry(uris[i],data[i],versionId,connection);
-    }
-  },(reason)=>{
-    throw reason;
-  });
+  var update = insert('version', { version: version }, connection)
+    .then((result) => {
+      //then iterate through each new data set and create the necessary tables.
+      var versionId = result.generated_keys[0];
+      for (var i = 0; i < uris.length; i++) {
+        updateVersionEntry(uris[i], data[i], versionId, connection);
+      }
+    }).catch((e) => {
+      throw e;
+    });
 
   // call the callback or return a promise.
-  if(callback){
+  if (callback) {
     update.then(callback());
   } else {
     return update;
@@ -45,110 +45,112 @@ function updateVersion(version, uris, data, connection,callback){
   the new data. It either calls the callback on completion or returns a 
   promise.
 */
-function updateVersionEntry(uri,data,versionId,connection,callback){
-    
-    // Does this uri exist?
-    var existingUri = r.db(dbName)
+function updateVersionEntry(uri, data, versionId, connection, callback) {
+
+  // Does this uri exist?
+  var existingUri = r.db(dbName)
     .table('uri')
-    .filter({uri:uri})
+    .filter({ uri: uri })
     .limit(1)
     .run(connection);
 
-    // Does this graph exist in the database already?
-    // currently a hard check. Should consider if this is intelligent!
-    var existingData = r.db(dbName)
+  // Does this graph exist in the database already?
+  // currently a hard check. Should consider if this is intelligent!
+  var existingData = r.db(dbName)
     .table('graph')
-    .filter({graph:data})
+    .filter({ graph: data })
     .limit(1)
     .run(connection);
 
-    var update = Promise.all([existingUri,existingData])
-    .then((result)=>{
+  var update = Promise.all([existingUri, existingData])
+    .then((result) => {
       Promise.all([result[0].toArray(), result[1].toArray()])
-      .then(([uriList,dataList])=>{
-        // Create a list of ids for existing data for this graph and this version
+        .then(([uriList, dataList]) => {
+          // Create a list of ids for existing data for this graph and this version
 
-        // If both already exist, check if they were linked in previous versions.
-        // If they were, all that need to be done is link the version into the existing network
-        // Otherwise, create a new uriTranslation to show the new linking
-        if(uriList.length && dataList.length){
-          // check if the point to the same place
-          return r.db(dbName)
-          .table('uriTranslation')
-          .filter({uri_id:uriList[0].id})
-          .filter({graph_id:dataList[0].id})
-          .count()
-          .run(connection)
-          .then((result)=>{
-            // result is the number of translation pivots that point to both the graph and the uri
-            // should only ever be 0 or 1.
-            if(!result){ // If it's 0 then this is a new linkage
-              return Promise.all([
-                insert('uriTranslation',{uri_id:uriList[0].id,graph_id:dataList[0].id},connection),
-                insert('versionTranslation',{version_id:versionId,graph_id:dataList[0].id},connection)
-              ]);
-            } else { // If it is one then it is a preexisting one.
-              return insert('versionTranslation',{version_id:versionId, graph_id:dataKist[0].id},connection);
-            }
-          }, (reason)=>{
-            throw reason;
-          })
-        
-        // If the uri existed before but now points to a new graph
-        // Create the new graph then link everything together/
-        } else if(uriList.length){
-          return insert('graph',{graph:data},connection)
-          .then((result)=>{
-            return Promise.all([
-              insert('uriTranslation', {uri_id:uriList[0].id,graph_id:result.generated_keys[0]},connection),
-              insert('versionTranslation', {version_id:versionId,graph_id:result.generated_keys[0]},connection)
-            ]);
-          }, (reason)=>{
-            throw reason;
-          });
-        
-        // If only the graph existed before (new uri for old graph),
-        // create the new uri and link everything together
-        } else if(dataList.length){
-          return insert('uri',{uri:uri},connection)
-          .then((result)=>{
-            return Promise.all([
-              insert('uriTranslation', {uri_id:result.generated_keys[0], graph_id:dataList[0].id}, connection),
-              insert('versionTranslation',{version_id:versionId, graph_id:dataList[0].id},connection)
-            ]);
-          }, (reason)=>{
-            throw reason
-          });
+          // If both already exist, check if they were linked in previous versions.
+          // If they were, all that need to be done is link the version into the existing network
+          // Otherwise, create a new uriTranslation to show the new linking
+          if (uriList.length && dataList.length) {
+            // check if the point to the same place
+            return r.db(dbName)
+              .table('uriTranslation')
+              .filter({ uri_id: uriList[0].id })
+              .filter({ graph_id: dataList[0].id })
+              .count()
+              .run(connection)
+              .then((result) => {
+                // result is the number of translation pivots that point to both the graph and the uri
+                // should only ever be 0 or 1.
+                if (!result) { // If it's 0 then this is a new linkage
+                  return Promise.all([
+                    insert('uriTranslation', { uri_id: uriList[0].id, graph_id: dataList[0].id }, connection),
+                    insert('versionTranslation', { version_id: versionId, graph_id: dataList[0].id }, connection)
+                  ]);
+                } else { // If it is one then it is a preexisting one.
+                  return insert('versionTranslation', { version_id: versionId, graph_id: dataList[0].id }, connection);
+                }
+              }).catch((e) => {
+                throw e;
+              });
 
-        // If none of the data existed before: create and link everything.
-        } else {
-          var graphProm = insert('graph', {graph:data},connection);
-          var uriProm = insert('uri',{uri:uri},connection);
+            // If the uri existed before but now points to a new graph
+            // Create the new graph then link everything together/
+          } else if (uriList.length) {
+            return insert('graph', { graph: data }, connection)
+              .then((result) => {
+                return Promise.all([
+                  insert('uriTranslation', { uri_id: uriList[0].id, graph_id: result.generated_keys[0] }, connection),
+                  insert('versionTranslation', { version_id: versionId, graph_id: result.generated_keys[0] }, connection)
+                ]);
+              }).catch((e) => {
+                throw e;
+              });
 
-          return Promise.all([graphProm,uriProm])
-          .then(([gResult,uResult])=>{
-            return Promise.all([
-              insert('uriTranslation',{uri_id:uResult.generated_keys[0],graph_id:gResult.generated_keys[0]},connection),
-              insert('versionTranslation',{version_id:versionId,graph_id:gResult.generated_keys[0]},connection)
-            ]);
-          },(reason)=>{
-            throw reason;
-          });
-          // Neither exist. Create everything
-        }
-      }, (reason)=>{
-        throw reason;
-      })
-    }, (reason)=>{
-      throw reason;
+            // If only the graph existed before (new uri for old graph),
+            // create the new uri and link everything together
+          } else if (dataList.length) {
+            return insert('uri', { uri: uri }, connection)
+              .then((result) => {
+                return Promise.all([
+                  insert('uriTranslation', { uri_id: result.generated_keys[0], graph_id: dataList[0].id }, connection),
+                  insert('versionTranslation', { version_id: versionId, graph_id: dataList[0].id }, connection)
+                ]);
+              }).catch((e) => {
+                throw e;
+              });
+
+            // If none of the data existed before: create and link everything.
+          } else {
+            var graphProm = insert('graph', { graph: data }, connection);
+            var uriProm = insert('uri', { uri: uri }, connection);
+
+            return Promise.all([graphProm, uriProm])
+              .then(([gResult, uResult]) => {
+                return Promise.all([
+                  insert('uriTranslation', { uri_id: uResult.generated_keys[0], graph_id: gResult.generated_keys[0] }, connection),
+                  insert('versionTranslation', { version_id: versionId, graph_id: gResult.generated_keys[0] }, connection)
+                ]);
+              }).catch((e) => {
+                throw e;
+              });
+          }
+        }).catch((e) => {
+          throw e;
+        });
+    }).catch((e) => {
+      throw e;
     });
 
-    // Return or callback depending on the input
-    if (callback){
-      update.then(callback());
-    } else {
-      return update;
-    }
+  // Return or callback depending on the input
+  if (callback) {
+    update.then(callback())
+      .catch((e) => {
+        throw e;
+      });
+  } else {
+    return update;
+  }
 }
 
 module.exports = {
